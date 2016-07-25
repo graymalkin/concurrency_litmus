8e1eb70016b576082ab834712cf294dc74bded4d
Simon Cooksey <sjc80@kent.ac.uk>
2016-07-25 13:59:35 +0100

    Added flags to change behaviour

--- src/main.c
+++ src/main.c
@@ -5,11 +5,11 @@
 #include "sb.h"
 #include "results.h"
 
-#define MAX_ITR 100000
 #define VERSION "0.0"
 
 void parse_params(int argc, char * argv[]);
 
+int max_itr = 100000;
 int quiet = 0;
 
 int
@@ -17,12 +17,12 @@ main(int argc, char * argv[])
 {
 	parse_params(argc, argv);
 
-	for(int i = 0; i < MAX_ITR; i++) {
+	for(int i = 0; i < max_itr; i++) {
 		if(sb_run(i) == RESULT_BEHAVIOUR_INCORRECT) {
 			printf("[SB]   Attempt %d showed weak behaviour\r\n", i);
 		}
-		if(quiet == 0 && i % (MAX_ITR / 1000)) {
-			printf("%.2f%%\r", ((float)i / MAX_ITR) * 100);
+		if(quiet == 0 && i % (max_itr / 1000)) {
+			printf("%.2f%%\r", ((float)i / max_itr) * 100);
 		}
 	}
 
@@ -37,9 +37,13 @@ parse_params(int argc, char * argv[])
 		if(strstr(argv[i], "-q")) {
 			quiet = 1;
 		}
-		if(strstr(argv[i],  "-v")) {
+		if(strstr(argv[i], "-v")) {
 				printf("Version: " VERSION "\r\n");
 				exit(0);
+		}
+		if(strstr(argv[i], "-I")) {
+			i++;
+			max_itr = atoi(argv[i]);
 		}
 		if(strstr(argv[i], "-h") || strstr(argv[i], "-?")) {
 			printf("Litmus Tests:\r\n");
